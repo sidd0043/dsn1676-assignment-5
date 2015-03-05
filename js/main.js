8515141c3a806dabade72a32855a73ace9eb7166
sidd0043 <Mukkadas@macbook-pro.gateway.2wire.net>
2015-03-05 12:12:03 -0500

    addedli and input

--- js/main.js
+++ js/main.js
@@ -6,3 +6,8 @@ var $li = $('.<li>');
 
 $form.on('submit', function (e) {
     e.preventDefault();
+
+    var $li = $('<li>').html($input.val());
+    var $inputDl = $('<button class="close xbutton ">x</button>');
+    
+    $inputDl.on
