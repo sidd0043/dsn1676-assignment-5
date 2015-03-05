94643c6e3c25c15b68322db2309e5c30acb91f71
sidd0043 <Mukkadas@macbook-pro.gateway.2wire.net>
2015-03-05 12:08:48 -0500

    added form submit

--- js/main.js
+++ js/main.js
@@ -1,59 +1,8 @@
-var $theCircle = $('.circle');
-var $moveDownBtn = $('.move-down');
-var $moveUpBtn = $('.move-up');
-var $moveLeftBtn = $('.move-left');
-var $moveRightBtn = $('.move-right');
-var $form = $('form');
-var $input = $('#the-color');
+var $form = $('.form');
+var $list = $('.list');
+var $input = $('.inpput');
+var $li = $('.<li>');
 
 
-
-//$theCircle.remove();
-//$theCircle.css('border', '3px solid black');
-
-$moveDownBtn.on('click', function () {
-    //$theCircle.addClass('highlight');
-    var currentlocation = $theCircle.offset();
-    console.log(currentlocation);
-    
-    $theCircle.css('top', currentlocation.top + 10);
-});
-
-$moveUpBtn.on('click', function () {
-    //$theCircle.addClass('highlight');
-    var currentlocation = $theCircle.offset();
-    console.log(currentlocation);
-    
-    $theCircle.css('top', currentlocation.top - 10);
-});
-
-$moveLeftBtn.on('click', function () {
-    //$theCircle.addClass('highlight');
-    var currentlocation = $theCircle.offset();
-    console.log(currentlocation);
-    
-    $theCircle.css('left', currentlocation.left + 10);
-});
-
-$moveRightBtn.on('click', function () {
-    //$theCircle.addClass('highlight');
-    var currentlocation = $theCircle.offset();
-    console.log(currentlocation);
-    
-    $theCircle.css('left', currentlocation.left - 10);
-
-});
-
-$form.on('submit', function(eventObject) {
-    eventObject.preventDefault();
-     $theCircle.css('background', $input.val());
-    
-    var $theH2 = $('<h2>');
-    $theH2.html($input.val());
-    $theCircle.append($theH2);
-    
-    $input.val('');
-    
-});
-     
-     
+$form.on('submit', function (e) {
+    e.preventDefault();
